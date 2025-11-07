48361a07b901371c65d2c5dc1b3ca1489d55b468
Fredrik von Post <fredrik.v.post@gmail.com>
2025-11-07 12:37:39 +0100

    Moved App and Test project to the same solution

---
+++ FreightCostCalculator.Tests/CentimeterTests.cs
@@ -0,0 +1,87 @@
+﻿using FreightCostCalculator.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreightCostCalculator.Tests
+{
+    public class CentimeterTests
+    {
+        [Fact]
+        public void CompareTo_Null_ReturnsOne()
+        {
+            var cm = new Centimeter(10);
+
+            int actual = cm.CompareTo((Centimeter?)null);
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void CompareTo_OtherSmaller_ReturnsPositive()
+            
+        {
+            var cm = new Centimeter(10);
+            var other = new Centimeter(5);
+
+            var actual = cm.CompareTo(other);
+
+            Assert.True(actual > 0);
+                }
+        [Fact]
+        public void CompareTo_OtherLarger_ReturnsNegative()
+        {
+            var cm = new Centimeter(5);
+            var other = new Centimeter(10);
+
+            var actual = cm.CompareTo(other);
+
+            Assert.True(actual < 0);
+        }
+
+        [Fact]
+        public void CompareTo_Equal_ReturnsZero()
+        {
+            var cm = new Centimeter(10);
+            var other = new Centimeter(10);
+
+            var actual = cm.CompareTo(other);
+
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void IComparable_CompareTo_ObjectCentimeter_MatchesGeneric()
+        {
+            var cm = new Centimeter(10);
+            var other = new Centimeter(5);
+
+            var expected = cm.CompareTo(other);
+            var actual = ((IComparable)cm).CompareTo(other);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IComparable_CompareTo_Null_ReturnsOne()
+        {
+            var cm = new Centimeter(10);
+
+            var actual = ((IComparable)cm).CompareTo(null);
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void IComparable_CompareTo_WrongType_ThrowsArgumentException()
+        {
+            var cm = new Centimeter(10);
+
+            Assert.Throws<ArgumentException>(() => ((IComparable)cm).CompareTo("not a centimeter"));
+        }
+    }
+
+}
+
---
+++ FreightCostCalculator.Tests/FreightCostCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using FreightCostCalculator.App;
+using System.Collections;
+
+namespace FreightCostCalculator.Tests;
+public class FreightCostCalculatorTests : FreightCostCalculatorBoxTests
+{
+       [Theory]
+       [ClassData(typeof(BoxFreightCostData))]
+    public void GetFreightCost_ReturnsExpected(int height, int width, int length, int weight, int expected)
+    {
+            Box sut = new(new Centimeter(height), new Centimeter(width), new Centimeter(length), new Kilogram(weight));
+            Ore expectedCost = new(expected);
+
+            var actualCost = sut.GetFreightCost();
+
+            Assert.Equal(expectedCost, actualCost);
+    }
+}
+
+public class BoxFreightCostData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+       yield return new object[] { 10, 5, 3, 1, 29_000}; //  _smallPackageLightWeightCost
+       yield return new object[] { 10, 10, 10, 11, 79_000}; //  _smallPackageHeavyWeightCost
+
+       yield return new object[] { 12, 7, 8, 5, 49_000 }; // _smallPackageMediumWeightCost
+       yield return new object[] { 12, 70, 80, 10, 12 * 80 * 10 + 10_000 };
+       yield return new object[] { 100, 100, 100, 20, 100 * 100 * 20 + 10_000 };
+            }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
