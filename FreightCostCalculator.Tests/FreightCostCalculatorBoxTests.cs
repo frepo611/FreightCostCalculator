7122e3673e5d212acc0097c4f68506ce617f2769
Fredrik von Post <fredrik.v.post@gmail.com>
2025-11-07 12:38:45 +0100

    Move Box constructor tests to a separat class

---
+++ FreightCostCalculator.Tests/FreightCostCalculatorBoxTests.cs
@@ -0,0 +1,22 @@
+﻿using FreightCostCalculator.App;
+
+namespace FreightCostCalculator.Tests
+{
+    public class FreightCostCalculatorBoxTests
+    {
+        [Theory]
+        [InlineData(-1, 1, 3, 10)]
+        [InlineData(10, 1, 3, 30)]
+        [InlineData(10, 1, 3, -30)]
+        [InlineData(10, 0, 3, 0)]
+        public void BoxConstructorShallThrowExceptionWhenParametersAreInvalid(int height, int width, int length, int weight)
+        {
+            Assert.ThrowsAny<Exception>(() =>
+            new Box(new Centimeter(height),
+            new Centimeter(width),
+            new Centimeter(length),
+            new Kilogram(weight)));
+
+        }
+    }
+}
